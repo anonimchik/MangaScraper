2f6d51d12037c8484cc5a38451f734209aa924ff
Эдуард Шавишов <edik2898@gmail.com>
2021-03-14 15:43:11 +0300

    14.03.21

---
+++ MangaScraper/Manhwa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MangaScraper
+{
+    class Manhwa
+    {
+        private string Title;
+        private byte VolumeNumber;
+        private ushort ChapterNumber;
+        private List<String> OtherTitles = new List<String>();
+        private string ManhwaStatus;
+        private string TranslateStatus;
+        private List<String> Genres = new List<String>();
+        private string Category;
+        private List<String> Screenweiters = new List<String>();
+        private List<String> Painters = new List<String>();
+        private ushort RealeaseYear;
+        private List<String> Other = new List<String>();
+        private List<String> Magazines = new List<String>();
+        private List<String> Tranlators = new List<String>();
+    }
+}
