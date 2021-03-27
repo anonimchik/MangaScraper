aa6f876a2bbaec6d1e97df29886f78c5e75a5d47
Office PC 1 <Office PC 1@WIN-BNFHUJPL2EF>
2021-03-27 07:13:29 +0300

    27.03

---
+++ MangaScraper/BaseContent.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MangaScraper
+{
+    class BaseContent
+    {
+        /// <summary>
+        /// Название манги
+        /// </summary>
+        public string Title;
+        /// <summary>
+        /// Список других названий манги
+        /// </summary>
+        public List<String> OtherTitles = new List<String>();
+        /// <summary>
+        /// Фоновое изображение
+        /// </summary>
+        public string BackgroundImg;
+        /// <summary>
+        /// Описание манги
+        /// </summary>
+        public string Description;
+        /// <summary>
+        /// Количество томов
+        /// </summary>
+        public byte VolumeNumber;
+        /// <summary>
+        /// Количество глав
+        /// </summary>
+        public ushort ChapterNumber;
+        /// <summary>
+        /// Статус перевода
+        /// </summary>
+        public string TranslateStatus;
+        /// <summary>
+        /// Художник
+        /// </summary>
+        public string Painter;
+        /// <summary>
+        /// Сценарист
+        /// </summary>
+        public string Screenweiter;
+        /// <summary>
+        /// Список глав
+        /// </summary>
+        public List<String> Chapters = new List<String>();
+        /// <summary>
+        /// Журнал
+        /// </summary>
+        public List<String> Magazines = new List<String>();
+        /// <summary>
+        /// Год выпуска
+        /// </summary>
+        public ushort ReleaseYear;
+        /// <summary>
+        /// Список жанров
+        /// </summary>
+        public List<String> Genres = new List<String>();
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<String> Tranlators = new List<String>();
+        /// <summary>
+        /// Список списков 
+        /// </summary>
+        public List<List<string>> Images = new List<List<string>>();
+
+    }
+}
--- MangaScraper/Manga.cs
+++ MangaScraper/Manga.cs
@@ -8,73 +8,8 @@ using OpenQA.Selenium.Support.UI;
 
 namespace MangaScraper
 {
-    class Manga
+    class Manga:BaseContent
     {
-        /// <summary>
-        /// Название манги
-        /// </summary>
-        public string Title;
-        /// <summary>
-        /// Список других названий манги
-        /// </summary>
-        public List<String> OtherTitles = new List<String>();
-        /// <summary>
-        /// Фоновое изображение
-        /// </summary>
-        public string BackgroundImg;
-        /// <summary>
-        /// Описание манги
-        /// </summary>
-        public string Description;
-        /// <summary>
-        /// Количество томов
-        /// </summary>
-        public byte VolumeNumber;
-        /// <summary>
-        /// Количество глав
-        /// </summary>
-        public ushort ChapterNumber;
-        /// <summary>
-        /// Статус перевода
-        /// </summary>
-        public string TranslateStatus;
-        /// <summary>
-        /// Художник
-        /// </summary>
-        public string Painter;
-        /// <summary>
-        /// Сценарист
-        /// </summary>
-        public string Screenweiter;
-        /// <summary>
-        /// Список глав
-        /// </summary>
-        public List<String> Chapters = new List<String>();
-        /// <summary>
-        /// Журнал
-        /// </summary>
-        public List<String> Magazines = new List<String>();
-        /// <summary>
-        /// Год выпуска
-        /// </summary>
-        public ushort ReleaseYear;
-        /// <summary>
-        /// Список жанров
-        /// </summary>
-        public List<String> Genres = new List<String>();
-        /// <summary>
-        /// 
-        /// </summary>
-        public List<String> Tranlators = new List<String>();
-        /// <summary>
-        /// Список списков 
-        /// </summary>
-        public List<List<string>> Images = new List<List<string>>();
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="chapter"></param>
-        /// <param name="drv"></param>
         public void parseMangaImageUrls(IWebDriver drv, Manga mng)
         {
             List<String> subImages = new List<String>();
@@ -94,6 +29,10 @@ namespace MangaScraper
 
         }
 
+        /// <summary>
+        /// Скачивание картинок
+        /// </summary>
+        /// <param name="MangaList"></param>
         public void downloadImages(List<Manga> MangaList)
         {
             for (int i = 0; i < MangaList.Count; i++)
@@ -114,7 +53,7 @@ namespace MangaScraper
         }
 
         /// <summary>
-        /// 
+        /// Парсинг информации о манге
         /// </summary>
         /// <param name="drv"></param>
         public void getMangaContent(IWebDriver drv, Manga mng)
