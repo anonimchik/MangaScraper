116030832e3d01a67d71d45a512e6924db46ecea
Эдуард Шавишов <edik2898@gmail.com>
2021-09-23 16:55:53 +0300

    23.09

--- MangaScraper/ViewModals/Manga24ViewModel.cs
+++ MangaScraper/ViewModals/Manga24ViewModel.cs
@@ -5,6 +5,10 @@ using Microsoft.Edge.SeleniumTools;
 using OpenQA.Selenium;
 using System.Text.RegularExpressions;
 using System.Net;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Collections.Specialized;
 
 namespace MangaScraper.ViewModals
 {
@@ -21,10 +25,15 @@ namespace MangaScraper.ViewModals
                 List<string> catalog_pages = new List<string>();
                 List<string> title_page = new List<string>();
                 BaseModel bm = new BaseModel();
+                /// Дописать алгоритм получения каталога всех тайтлов
                 drv.Navigate().GoToUrl(SourceUrl);
+                ///
                 get_title_info(drv, bm);
                 get_chapter_info(drv, bm);
                 get_images_from_chapters(drv, bm);
+                download_images_to_local_storage(bm);
+                create_directories_on_server(bm);
+                upload_images_to_server(bm);
             }
         }
 
@@ -78,7 +87,9 @@ namespace MangaScraper.ViewModals
             drv.FindElements(By.XPath("//td[@class='translator']")).CopyTo(translator, 0);
             IWebElement[] link = new IWebElement[drv.FindElements(By.XPath("//a[@class='chapter-link']")).Count];
             drv.FindElements(By.XPath("//a[@class='chapter-link']")).CopyTo(link, 0);
-            for(int i = 0; i < chapter_number.Length; i++) //доделать скролинг таблицы с главами
+            IJavaScriptExecutor js = (IJavaScriptExecutor)drv;
+            js.ExecuteScript("document.getElementsByClassName('table-wrap ps')[0].style.maxHeight='inherit'");
+            for (int i = 0; i < chapter_number.Length; i++) //доделать скролинг таблицы с главами
             {
                 bm.Chapters.Add(link[i].GetAttribute("href") + "|" + chapter_number[i].Text + "|" + chapter_name[i].Text + "|" + translator[i].Text);
                 break;
@@ -94,7 +105,6 @@ namespace MangaScraper.ViewModals
         public void get_images_from_chapters(IWebDriver drv, BaseModel bm)
         {
             //обрезание строки и получение названия картинки https://manga24.ru/Content/pages/onepiece/1025/op_1024_000a.jpg -> 000а.jpg
-
             for (int i = 0; i < bm.Chapters.Count; i++) //проход по главам
             {
                 List<string> sub_images = new List<string>();
@@ -160,14 +170,83 @@ namespace MangaScraper.ViewModals
 
                 }
                 bm.Images.Add(new List<string>(sub_images));
+                //break;
+            }
+
+        }
+
+        public void create_directories_on_server(BaseModel bm)
+        {
+            string json = "",
+                response = "";
+            string url = "http://95.54.44.39:6001/MyWeb/SpaceManga/Scraper/scraper.php"; //исполняемый файл;
+            var options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true
+            };
+            json = JsonSerializer.Serialize(bm.PathImages, options); //формирование json строки для отправки на сервер
+            using (var webClient = new WebClient())
+            {
+                var pars = new NameValueCollection();
+                pars.Add("dir", json); //post данные 
+                response = Regex.Replace(Encoding.UTF8.GetString(webClient.UploadValues(url, pars)), @"\W", ""); //удаление лишнего в ответе сервера
+            }
+        }
+        public void upload_images_to_server(BaseModel bm)
+        {
+            for (int i = 0; i < bm.Images.Count; i++)
+            {
+                for (int j = 0; j < bm.Images[i].Count; j++)
+                {
+                    var expansion = bm.Images[i][j].IndexOf(".jpeg") == 1 ? ".jpeg" : ".png";
+                    FtpWebRequest req = (FtpWebRequest)WebRequest.Create("ftp://95.54.44.39/SpaceManga/Titles/" + bm.PathImages[i][j] + "/" + (j + 1).ToString() + expansion);
+                    req.Credentials = new NetworkCredential("admin", "64785839");
+                    req.Method = WebRequestMethods.Ftp.UploadFile;
+                    try
+                    {
+                        using (Stream fileStream = File.OpenRead(@"A:MangaScraper/" + bm.PathImages[i][j] + "/" + (j + 1).ToString() + expansion))
+                        using (Stream ftpStream = req.GetRequestStream())
+                        {
+                            fileStream.CopyTo(ftpStream);
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
                 break;
             }
 
         }
 
-        public void download_images_to_local_storage()
+        public void download_images_to_local_storage(BaseModel bm)
         {
-
+            WebClient client = new WebClient();
+            for (int i = 0; i < bm.Images.Count; i++)
+            {
+                string jpg = "",
+                       png = "",
+                       exp = "";
+                List<String> expansion = new List<String>();
+                for (int j = 0; j < bm.Images[i].Count; j++)
+                {
+                    exp = (bm.Images[i][j].IndexOf(".jpg")) > -1 ? ".jpg" : ".png";
+                    DirectoryInfo dir = new DirectoryInfo("A:MangaScraper/" + bm.PathImages[i][j] + "/");
+                    if (!dir.Exists) dir.Create();
+                    if (bm.Images[i][j].IndexOf(".jpg") > -1)
+                    {
+                        jpg += (j + 1) + ",";
+                    }
+                    if (bm.Images[i][j].IndexOf(".png") > -1)
+                    {
+                        png += (j + 1) + ",";
+                    }
+                    client.DownloadFile(bm.Images[i][j], "A:MangaScraper/" + bm.PathImages[i][j] + "/" + (j + 1).ToString() + exp);
+                    //break;
+                }
+                expansion.Add(Regex.Replace(jpg, @"\,$", "") + "|" + Regex.Replace(png, @"\,$", ""));
+                bm.Expansion.Add(new List<String>(expansion));
+                break;
+            }
         }
     }
 }
